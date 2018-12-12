59929d429d8e4729d616f560d138e53c6b82d3be
Michael Rychly <michaelrychly@gmail.com>
2018-12-11 19:42:21 -0500

    added firebase to index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, combineReducers} from 'redux';
 import {createLogger} from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
+import firebase from 'firebase';
 import './index.css';
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker';
@@ -12,6 +13,16 @@ import 'tachyons';
 const logger = createLogger();
 const rootReducer = combineReducers({})
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware,logger))
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBf7bB3sdrfqbDp26bmxoP3Qm8A5_KgyDs",
+  authDomain: "battlecity-fcea8.firebaseapp.com",
+  databaseURL: "https://battlecity-fcea8.firebaseio.com",
+  projectId: "battlecity-fcea8",
+  storageBucket: "",
+  messagingSenderId: "539299225290"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
 <Provider store={store}>
