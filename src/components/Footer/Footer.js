bd76e2568b7ab80b954606eeab1a062dcf20a38c
Kirapan <pxqpxqpxq@hotmail.com>
2019-01-22 20:31:26 -0500

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Footer.css';
+
+
+const footer = (props) => ( 
+    <div className='Footer'>footer</div>
+);
+
+export default footer;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
+import Footer from '../../components/Footer/Footer';
 import './Layout.css';
 
 const layout = (props) => ( 
@@ -8,6 +9,7 @@ const layout = (props) => (
       <main className='content'>
         {props.children}
       </main>
+      <Footer />
     </>
 );
 
