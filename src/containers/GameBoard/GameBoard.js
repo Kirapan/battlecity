8deaa0210b32e0274c986c17094bdd05602fd133
晓奇潘 <xiaoqipan@hans-MacBook-Pro.local>
2019-02-13 20:29:33 -0500

    added state

--- src/containers/GameBoard/GameBoard.js
+++ src/containers/GameBoard/GameBoard.js
@@ -1,14 +1,37 @@
 import React, {Component} from 'react';
 import Login from '../../containers/Login/Login';
+import tank from '../../assets/images/tank.png'
 
 import './GameBoard.css';
 
 class GameBoard extends Component {
   state = {
-    loggedIn: true
+    loggedIn: true,
+    gameState: 'PAUSE',
+    score: 0,
+    enermies: [1, 1, 1, 1, 1],
+    killed: 0,
+    grid: [
+      [null, null, null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null, null, null]
+    ],
+    tankPosition: {
+      x: 0,
+      y: 0
+    },
   }
   render () {
-    return this.state.loggedIn? (<div className='GameBoard'>hello</div>) : <Login />
+    const image = (<img src={tank} alt='tank' height='50' width='50'/>)
+    
+    return this.state.loggedIn? (<div className='GameBoard'>{image}</div>) : <Login />
   }
 }
 
