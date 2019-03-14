1334e7dff4f3d751bc0a02fdbae3efcc2bb03715
晓奇潘 <xiaoqipan@hans-MacBook-Pro.local>
2019-03-13 20:22:50 -0400

    added keydown event

--- src/containers/GameBoard/GameBoard.js
+++ src/containers/GameBoard/GameBoard.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import Login from '../../containers/Login/Login';
 import tank from '../../assets/images/tank.png'
 
@@ -21,17 +22,51 @@ class GameBoard extends Component {
       [null, null, null, null, null, null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null, null, null, null, null, null]
     ],
     tankPosition: {
       x: 0,
       y: 0
     },
+    style: {
+      position: 'absolute',
+      left: '',
+      top:'',
+      transform: ''
+    }
   }
+
+  componentDidMount(){
+    document.addEventListener("keydown", this.keyDownHandler, false);
+  }
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.keyDownHandler, false);
+  }
+
+  keyDownHandler = (e) => {
+    if(e.keyCode === 37) {
+      console.log('i am left arrow');
+    } else if (e.keyCode === 38) {
+      console.log('i am up arrow')
+    } else if (e.keyCode === 39) {
+      console.log('i am right arrow')
+    } else if (e.keyCode === 40) {
+      console.log('i am down arrow')
+    }
+  }
+
   render () {
-    const image = (<img src={tank} alt='tank' height='50' width='50'/>)
+    const image = (<img src={tank} alt='tank' height='40px' width='50px'/>)
     
-    return this.state.loggedIn? (<div className='GameBoard'>{image}</div>) : <Login />
+    return this.state.loggedIn? 
+    (
+    <div className='GameBoard'>
+      <div className='tankInPlay' style={this.state.style} onKeyDown={this.keyDownHandler}>{image}</div>
+    </div>) 
+    : <Login />
   }
 }
 
